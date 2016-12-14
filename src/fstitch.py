e20bc451b84ec8a92501d3739a91f6c5e1bd2875
Jonathan Rubin <jonathanrubin@rl1-192-189-dhcp.int.colorado.edu>
2016-12-14 12:06:55 -0700

    updates

--- src/fstitch.py
+++ src/fstitch.py
@@ -9,8 +9,8 @@ def run(fstitchdir,trainingdir,bedgraphs,fstitchbed):
     trainingfile = trainingdir + 'training_set_fixed.txt'
     for file1 in bedgraphs:
         filename = file1.split('/')[-1]
-        os.system(fstitchdir + "train -i " + file1 + " -j " + trainingfile + " -o " + trainingdir + filename + "_parameters.out") 
-        os.system(fstitchdir + "segment -i " + file1 + " -k " + trainingdir + filename + "_parameters.out" + " -o " + fstitchbed + filename + ".bed")
+        os.system(fstitchdir + " train -i " + file1 + " -j " + trainingfile + " -o " + trainingdir + filename + "_parameters.out") 
+        os.system(fstitchdir + " segment -i " + file1 + " -k " + trainingdir + filename + "_parameters.out" + " -o " + fstitchbed + filename + ".bed")
 
 
 
