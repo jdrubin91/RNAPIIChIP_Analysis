fbcd7427ec412cd638f094a066e388b425210ea7
Jonathan Rubin <jonathanrubin@rl1-192-53-dhcp.int.colorado.edu>
2016-12-20 11:08:00 -0700

    updates

--- src/deseq.py
+++ src/deseq.py
@@ -47,7 +47,7 @@ def run(counts,conditions,deseqdir):
     F = plt.figure() 
     ax = F.add_subplot(111)
     plt.scatter(x,y,c='b',edgecolor="",s=14)
-    ax.xscale('log')
+    plt.set_xscale('log')
     plt.savefig(deseqdir + 'MA_plot.png')
 
             
