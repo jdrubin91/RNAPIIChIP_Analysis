509d70fbc94afa04bf046df852a43a25617088b4
Jonathan Rubin <jonathanrubin@rl1-195-177-dhcp.int.colorado.edu>
2016-12-21 11:00:15 -0700

    updates

--- src/deseq.py
+++ src/deseq.py
@@ -3,7 +3,9 @@ __author__ = 'Jonathan Rubin'
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import itertools
 import numpy as np
+from scipy import stats
 
 def run(counts,conditions,deseqdir):
     d = dict()
@@ -20,38 +22,84 @@ def run(counts,conditions,deseqdir):
         print "Error: More than two conditions. All replicates should be labeled the same."
     x = list()
     y = list()
+    sites = list()
     with open(counts) as F:
         F.readline()
         for line in F:
             line = line.strip().split()
             site = ':'.join(line[:3])
-            d[site] = list()
+            d[site] = [[]]
             for val in line[3:]:
-                #d[chr:start:stop] = [val1,val2,val3,val4,...,valn,condition1mean,var1,condition2mean,var2,meanexpression,log2foldchange]
-                d[site].append(float(val))
-            for indexlist in conditionIndexes:
-                values = list()
-                for i in indexlist:
-                    values.append(d[site][i])
-                d[site].append(np.mean(values))
-                d[site].append(np.var(values))
-            meanexpression = np.mean([d[site][-4],d[site][-2]])
-            if d[site][-4] == 0 or d[site][-2] == 0:
+                #d[chr:start:stop] = [[val1,val2,val3,val4,...,valn],[log2foldchangeiterations],condition1mean,condition2mean,meanexpression,log2foldchangemean]
+                d[site][0].append(float(val))
+            valuesi = conditionIndexes[0]
+            valuesj = conditionIndexes[1]
+            values = list()
+            for r in itertools.product(valuesi, valuesj):
+                if r[0] == 0 or r[1] == 0:
+                    values.append(0.0)
+                else:
+                    values.append(np.log2(r[1]/r[0]))
+            d[site].append(values)
+            condition1mean = np.mean(valuesi)
+            condition2mean = np.mean(valuesj)
+            d[site].append(condition1mean)
+            d[site].append(condition2mean)
+            meanexpression = np.mean([condition1mean,condition2mean])
+            if condition1mean == 0 or condition2mean == 0:
                 foldchange = 0.0
             else:
-                foldchange = np.log2(d[site][-2]/d[site][-4])
+                foldchange = np.log2(condition2mean/condition1mean)
             x.append(meanexpression) 
             d[site].append(meanexpression)
             y.append(foldchange)
             d[site].append(foldchange)
+            sites.append(site)
+
+
+    sigx = list()
+    sigy = list()
+
+    low = min(x)
+    high = max(x)
+    windows = int(np.log10(high))
+    p = 0.05
+    for i in range(windows):
+        j = (i*10)+low
+        k = (i+low)*10 if (i+low)*10 < high else high
+        windowx = list()
+        windowy = list()
+        keys = list()
+        for l in range(len(x)):
+            if j < x[l] < k:
+                keys.append(sites[l])
+                windowx.append(x[l])
+                windowy.append(y[l])
+        
+        for key in keys:
+            replist = d[key][1]
+            pval = stats.ks_2samp(windowy, replist)[1]
+            d[key].append(pval)
+            if pval < p:
+                sigx.append(d[key][-2])
+                sigy.append(d[key][-1])
+
+
+
+
+
+
+
     F = plt.figure() 
     ax = F.add_subplot(111)
     plt.xscale('log')
     plt.scatter(x,y,c='b',edgecolor="",s=14)
+    plt.scatter(sigx,sigy,c='r',edgecolor="",s=14)
     plt.title(conditionNames[1] + ' vs. ' + conditionNames[0])
     plt.ylabel("log2(" + conditionNames[1] + "/" + conditionNames[0] + ")")
     plt.xlabel("Mean Expression")
     plt.savefig(deseqdir + 'MA_plot.png')
+
 
             
 
